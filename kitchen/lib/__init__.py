69b816868337683a7dd90f24711e03c5eb982416
Aengus Walton <ventolin@gmail.com>
2012-03-22 12:57:11 +0100

    Use a sortable list instead of a dictionary of values for the return value

--- kitchen/lib/__init__.py
+++ kitchen/lib/__init__.py
@@ -4,14 +4,16 @@ import json
 from kitchen.settings import KITCHEN_LOCATION
 
 def load_data(data_type):
-    retval = {}
+    retval = []
     nodes_dir = os.path.join(KITCHEN_LOCATION, data_type)
     if not os.path.isdir(nodes_dir):
         raise IOError('Invalid data type or kitchen location. Check your settings.')
     for filename in os.listdir(nodes_dir):
         if filename.endswith('.json'):
+            entry = {'name': filename[:-5]}
             f = open(os.path.join(nodes_dir, filename), 'r')
-            retval[filename[:-5]] = json.load(f)
+            entry['data'] = json.load(f)
             f.close()
+            retval.append(entry)
     return retval
 
