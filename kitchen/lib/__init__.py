d87b68d61561dcb242379dfd59234ae2bd5fb7c2
Aengus Walton <ventolin@gmail.com>
2012-03-22 12:19:48 +0100

    Abstract load_nodes() to load_data(), being able to pass in an argument of what type of data (nodes, roles, etc) to load

--- kitchen/lib/__init__.py
+++ kitchen/lib/__init__.py
@@ -3,11 +3,15 @@ import json
 
 from kitchen.settings import KITCHEN_LOCATION
 
-def load_nodes():
+def load_data(data_type):
     retval = {}
-    nodes_dir = os.path.join(KITCHEN_LOCATION, 'nodes')
+    nodes_dir = os.path.join(KITCHEN_LOCATION, data_type)
+    if not os.path.isdir(nodes_dir):
+        raise IOError('Invalid data type or kitchen location. Check your settings.')
     for filename in os.listdir(nodes_dir):
-        f = open(os.path.join(nodes_dir, filename), 'r')
-        retval[filename[:-5]] = json.load(f)
-        f.close()
+        if filename.endswith('.json'):
+            f = open(os.path.join(nodes_dir, filename), 'r')
+            retval[filename[:-5]] = json.load(f)
+            f.close()
     return retval
+
