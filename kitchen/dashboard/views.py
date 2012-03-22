ace875173f8a2ad428a616fcdfa8fefd1bbf576d
Aengus Walton <ventolin@gmail.com>
2012-03-22 12:57:54 +0100

    Added basic view

--- kitchen/dashboard/views.py
+++ kitchen/dashboard/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+from kitchen.lib import load_data
+
+def main(request):
+    return HttpResponse(render_to_string('main.html', {'nodes': load_data('nodes')}))
