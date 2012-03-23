6724e1c05c15bab93f7fad8523544400907e7e12
Miquel Torres <tobami@googlemail.com>
2012-03-23 15:45:22 +0100

    Refactor views.get_data() so that it only returns one big data dictionary

--- kitchen/dashboard/views.py
+++ kitchen/dashboard/views.py
@@ -7,9 +7,11 @@ from kitchen.settings import REPO, SHOW_VIRT_VIEW
 
 
 def get_data(request):
-    filter_env = request.GET.get('env', '')
-    filter_roles = request.GET.get('roles','')
-    filter_virt = request.GET.get('virt','')
+    data = {
+        'filter_env': request.GET.get('env', ''),
+        'filter_roles': request.GET.get('roles',''),
+        'filter_virt': request.GET.get('virt',''),
+    }
     nodes = get_nodes_extended()
     roles = get_roles()
     environments = [] # an 'implicit' set, as environments must be uniquely named
@@ -22,44 +24,47 @@ def get_data(request):
                                  'count': len([node for node in nodes if node['chef_environment'] == name])})
         else:
             roles_groups.add(split[0])
-    virt_roles = ['host', 'guest']
+    data['virt_roles'] = ['host', 'guest']
     # Filter nodes
-    if filter_env or filter_roles or filter_virt:
+    if data['filter_env'] or data['filter_roles'] or data['filter_virt']:
         nodes = filter_nodes(
-            filter_env, filter_roles, filter_virt, get_nodes_extended())
-    return nodes, roles, roles_groups, environments, virt_roles, filter_env,\
-        filter_roles, filter_virt
+            data['filter_env'], data['filter_roles'], data['filter_virt'], get_nodes_extended())
+    data['nodes'] = nodes
+    data['roles'] = roles
+    data['roles_groups'] = roles_groups
+    data['environments'] = environments
+    return data
 
 
 def main(request):
-    nodes, roles, roles_groups, environments, virt_roles, filter_env, \
-        filter_roles, filter_virt = get_data(request)
-    return HttpResponse(render_to_string('main.html',
-                                        {'nodes': nodes,
-                                         'roles': roles,
-                                         'roles_groups': sorted(roles_groups),
-                                         'environments': sorted(environments),
-                                         'virt_roles': virt_roles,
-                                         'filter_env': filter_env,
-                                         'filter_roles': filter_roles,
-                                         'filter_virt': filter_virt,
-                                         'show_virt': SHOW_VIRT_VIEW}))
+    data = get_data(request)
+    return HttpResponse(
+        render_to_string('main.html',
+                        {'nodes': data['nodes'],
+                        'roles': data['roles'],
+                        'roles_groups': sorted(data['roles_groups']),
+                        'environments': sorted(data['environments']),
+                        'virt_roles': data['virt_roles'],
+                        'filter_env': data['filter_env'],
+                        'filter_roles': data['filter_roles'],
+                        'filter_virt': data['filter_virt'],
+                        'show_virt': SHOW_VIRT_VIEW}))
 
 
 def graph(request):
-    nodes, roles, roles_groups, environments, virt_roles, filter_env, \
-        filter_roles, filter_virt = get_data(request)
+    data = get_data(request)
     msg = ""
     if not request.GET.get('env'):
-        nodes = []
+        data['nodes'] = []
         msg = "Please select an environment"
-    graphs.generate_node_map(nodes)
-    return HttpResponse(render_to_string('graph.html',
-                                        {'nodes': nodes,
-                                         'roles': roles,
-                                         'roles_groups': sorted(roles_groups),
-                                         'environments': sorted(environments),
-                                         'filter_env': filter_env,
-                                         'filter_roles': filter_roles,
-                                         'msg': msg,
-                                         }))
+    graphs.generate_node_map(data['nodes'])
+    return HttpResponse(
+        render_to_string('graph.html',
+                        {'nodes': data['nodes'],
+                        'roles': data['roles'],
+                        'roles_groups': sorted(data['roles_groups']),
+                        'environments': sorted(data['environments']),
+                        'filter_env': data['filter_env'],
+                        'filter_roles': data['filter_roles'],
+                        'msg': msg,
+                        }))
