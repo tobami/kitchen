9a30728493258d7dcf60b67a8c87489e1457df1a
Darío Blanco <dario.blanco@edelight.de>
2012-10-23 16:50:56 +0200

    Check 'role_list' before sending it to little_chef

--- kitchen/dashboard/templatetags/filters.py
+++ kitchen/dashboard/templatetags/filters.py
@@ -10,16 +10,23 @@ register = template.Library()
 @register.filter(name='get_role_list')
 def get_role_list(run_list):
     """Returns the role sublist from the given run_list"""
-    prev_role_list = littlechef.lib.get_roles_in_node({'run_list': run_list})
-    role_list = []
-    for role in prev_role_list:
-        if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
-            # Only add if it doesn't start with excluded role prefixes
-            role_list.append(role)
-    return role_list
+    if run_list:
+        all_roles = littlechef.lib.get_roles_in_node(
+            {'run_list': run_list})
+        role_list = []
+        for role in all_roles:
+            if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
+                # Only add if it doesn't start with excluded role prefixes
+                role_list.append(role)
+        return role_list
+    else:
+        return []
 
 
 @register.filter(name='get_recipe_list')
 def get_recipe_list(run_list):
     """Returns the recipe sublist from the given run_list"""
-    return littlechef.lib.get_recipes_in_node({'run_list': run_list})
+    if run_list:
+        return littlechef.lib.get_recipes_in_node({'run_list': run_list})
+    else:
+        return []
