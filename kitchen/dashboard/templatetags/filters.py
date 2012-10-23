2331f2514ef0641fdb29afddf99b1f6aef75bdce
Darío Blanco <dario.blanco@edelight.de>
2012-10-23 16:37:08 +0200

    Use little chef in django filters for run_list

--- kitchen/dashboard/templatetags/filters.py
+++ kitchen/dashboard/templatetags/filters.py
@@ -1,5 +1,6 @@
 """Dashboard template filters"""
 from django import template
+import littlechef
 
 from kitchen.settings import REPO
 
@@ -9,23 +10,16 @@ register = template.Library()
 @register.filter(name='get_role_list')
 def get_role_list(run_list):
     """Returns the role sublist from the given run_list"""
+    prev_role_list = littlechef.lib.get_roles_in_node({'run_list': run_list})
     role_list = []
-    if run_list:
-        for element in run_list:
-            if "role[" in element:
-                role = element.split("role[")[1][:-1]
-                if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
-                    # Only add if it doesn't start with excluded role prefixes
-                    role_list.append(role)
+    for role in prev_role_list:
+        if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
+            # Only add if it doesn't start with excluded role prefixes
+            role_list.append(role)
     return role_list
 
 
 @register.filter(name='get_recipe_list')
 def get_recipe_list(run_list):
     """Returns the recipe sublist from the given run_list"""
-    recipe_list = []
-    if run_list:
-        for element in run_list:
-            if "recipe[" in element:
-                recipe_list.append(element.split("recipe[")[1][:-1])
-    return recipe_list
+    return littlechef.lib.get_recipes_in_node({'run_list': run_list})
