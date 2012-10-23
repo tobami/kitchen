4ca906b34e4870a80283653be7d3e092cb3e4a51
Darío Blanco <dario.blanco@edelight.de>
2012-10-23 17:59:27 +0200

    Simplify role template filter

--- kitchen/dashboard/templatetags/filters.py
+++ kitchen/dashboard/templatetags/filters.py
@@ -10,17 +10,12 @@ register = template.Library()
 @register.filter(name='get_role_list')
 def get_role_list(run_list):
     """Returns the role sublist from the given run_list"""
-    if run_list:
-        all_roles = littlechef.lib.get_roles_in_node(
-            {'run_list': run_list})
-        role_list = []
-        for role in all_roles:
-            if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
-                # Only add if it doesn't start with excluded role prefixes
-                role_list.append(role)
-        return role_list
-    else:
-        return []
+    role_list = []
+    for role in littlechef.lib.get_roles_in_node({'run_list': run_list or []}):
+        if not role.startswith(REPO['EXCLUDE_ROLE_PREFIX']):
+            # Only add if it doesn't start with excluded role prefixes
+            role_list.append(role)
+    return role_list
 
 
 @register.filter(name='get_recipe_list')
