072ffb42d21b9378cfab64ba0b426a82ed8bb27d
Darío Blanco <dario.blanco@edelight.de>
2012-10-30 15:34:39 +0100

    Refactor kitchen.js

--- kitchen/dashboard/static/js/kitchen.js
+++ kitchen/dashboard/static/js/kitchen.js
@@ -1,3 +1,25 @@
+function getDefaultDataTableConfig() {
+    return {
+        "bPaginate": false,
+        "oLanguage": {
+            "sSearch": "",
+            "sInfo" : "Showing _TOTAL_ nodes",
+            "sInfoEmpty": "No nodes found",
+            "sInfoFiltered": " (filtering from _MAX_ total)",
+            "sZeroRecords": "No nodes to display"
+        },
+        "oSearch": {
+            "sSearch": getSearchText(),
+        },
+        "aoColumnDefs": [
+            /* Expander */ { "bSortable": false, "aTargets": [0] },
+            /* Fqdn */ { "bVisible": false, "aTargets": [1] }
+        ],
+        "sDom": '<"top"if>rt<"bottom"lp><"clear">'
+    }
+}
+
+
 function getSearchText() {
     /*
      * Gets the predefined search text from the current page url
@@ -13,24 +35,7 @@ function drawNodeListTable(searchText) {
     /*
      * Creates a list of nodes DataTable
      */
-    oTable = $('#nodes').dataTable({
-        "bPaginate": false,
-        "oLanguage": {
-            "sSearch": "",
-            "sInfo" : "Showing _TOTAL_ nodes",
-            "sInfoEmpty": "No nodes found",
-            "sInfoFiltered": " (filtering from _MAX_ total)",
-            "sZeroRecords": "No nodes to display"
-        },
-        "oSearch": {
-            "sSearch": searchText,
-        },
-        "aoColumnDefs": [
-            /* Expander */ { "bSortable": false, "aTargets": [0] },
-            /* Fqdn */ { "bVisible": false, "aTargets": [1] }
-        ],
-        "sDom": '<"top"if>rt<"bottom"lp><"clear">'
-    });
+    oTable = $('#nodes').dataTable(getDefaultDataTableConfig());
     setSearchBox();
     setExtendedRows(oTable);
 }
@@ -39,50 +44,35 @@ function drawNodeVirtTable(searchText) {
     /*
      * Creates the guests DataTable, grouped by hosts
      */
-    oTable = $('#nodes').dataTable({
-        "fnDrawCallback": function (oSettings) {
-            if (oSettings.aiDisplay.length == 0) {
-                return;
+    // Extend the default DataTable config
+    dataTableConfig = getDefaultDataTableConfig();
+    dataTableConfig['aoColumnDefs'].push({ "bVisible": false, "aTargets": [2] });  // Grouper column
+    dataTableConfig['aaSortingFixed'] = [[ 2, 'asc' ]];
+    dataTableConfig['aaSorting'] = [[ 3, 'asc' ]];
+    dataTableConfig['fnDrawCallback'] = function (oSettings) {
+        if (oSettings.aiDisplay.length == 0) {
+            return;
+        }
+        var nTrs = $('#nodes tbody tr');
+        var iColspan = nTrs[0].getElementsByTagName('td').length;
+        var sLastGroup = "";
+        for (var i=0;i<nTrs.length;i++) {
+            var iDisplayIndex = oSettings._iDisplayStart + i;
+            var sGroup = oSettings.aoData[oSettings.aiDisplay[iDisplayIndex]]._aData[2];
+            if (sGroup != sLastGroup) {
+                var nGroup = document.createElement('tr');
+                var nCell = document.createElement('td');
+                nCell.colSpan = iColspan;
+                nCell.id = "host_grouper";
+                nCell.innerHTML = sGroup;
+                nGroup.appendChild(nCell);
+                nTrs[i].parentNode.insertBefore(nGroup, nTrs[i]);
+                sLastGroup = sGroup;
             }
-            var nTrs = $('#nodes tbody tr');
-            var iColspan = nTrs[0].getElementsByTagName('td').length;
-            var sLastGroup = "";
-            for (var i=0;i<nTrs.length;i++) {
-                var iDisplayIndex = oSettings._iDisplayStart + i;
-                var sGroup = oSettings.aoData[oSettings.aiDisplay[iDisplayIndex]]._aData[2];
-                if (sGroup != sLastGroup) {
-                    var nGroup = document.createElement('tr');
-                    var nCell = document.createElement('td');
-                    nCell.colSpan = iColspan;
-                    nCell.id = "host_grouper";
-                    nCell.innerHTML = sGroup;
-                    nGroup.appendChild(nCell);
-                    nTrs[i].parentNode.insertBefore(nGroup, nTrs[i]);
-                    sLastGroup = sGroup;
-                }
-            }
-        },
-        "bPaginate": false,
-        "oLanguage": {
-            "sSearch": "",
-            "sInfo" : "Showing _TOTAL_ nodes",
-            "sInfoEmpty": "No nodes found",
-            "sInfoFiltered": " (filtering from _MAX_ total)",
-            "sZeroRecords": "No nodes to display"
-        },
-        "oSearch": {
-            "sSearch": searchText,
-        },
-        "aoColumnDefs": [
-            /* Expander */ { "bSortable": false, "aTargets": [0] },
-            /* Fqdn */ { "bVisible": false, "aTargets": [1] },
-            /* Grouper */ { "bVisible": false, "aTargets": [2] }
-        ],
-        "aaSortingFixed": [[ 2, 'asc' ]],
-        "aaSorting": [[ 3, 'asc' ]],
-        "sDom": '<"top"if>rt<"bottom"lp><"clear">'
-    });
-
+        }
+    };
+    console.log(dataTableConfig);
+    oTable = $('#nodes').dataTable(dataTableConfig);
     setSearchBox();
     setExtendedRows(oTable);
 }
