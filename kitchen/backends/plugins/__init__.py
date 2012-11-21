0ed5658669a5832f255efb02ab77b188de35c2ef
Markus Korn <markus.korn@edelight.de>
2012-11-21 09:40:43 +0100

    Fixed typo in is_view() docstring Added 'Markus Korn' to NOTICE

--- kitchen/backends/plugins/__init__.py
+++ kitchen/backends/plugins/__init__.py
@@ -4,7 +4,7 @@ from kitchen.backends.plugins.loader import import_plugins
 
 def is_view(func):
     """Decorator to mark a plugin method as being a view.
-    Views can be called via the '/plugins/' insterface of kitchen and should
+    Views can be called via the '/plugins/' interface of kitchen and should
     either return None or a proper Django HTTPResponse object
 
     """
