d88877e0bd17932dfbf6ae64c563d15199c60ce7
Markus Korn <markus.korn@edelight.de>
2012-11-22 11:16:59 +0100

    kitchen.backend.plugins: changed parameter name from 'caps' to 'plugin_type' to be more descriptive

--- kitchen/backends/plugins/__init__.py
+++ kitchen/backends/plugins/__init__.py
@@ -2,20 +2,20 @@
 from kitchen.backends.plugins.loader import import_plugins
 
 
-def is_view(caps=None):
+def is_view(plugin_type):
     """Decorator to mark a plugin method as being a view.
     Views can be called via the '/plugins/' interface of kitchen and should
     either return None or a proper Django HTTPResponse object
 
     """
-    if callable(caps):
-        caps.__is_view__ = True
-        caps.__p_type__ = 'list'
-        return caps
+    if callable(plugin_type):
+        plugin_type.__is_view__ = True
+        plugin_type.__p_type__ = 'list'
+        return plugin_type
     else:
         def inner(func):
             func.__is_view__ = True
-            func.__p_type__ = caps
+            func.__p_type__ = plugin_type
             return func
         return inner
 
